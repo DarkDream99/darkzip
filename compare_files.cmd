ce12da4c30810daa854360e8c2a39b9342222f5a
DarkDream99 <denys.zaluzhnyi@gmail.com>
2019-01-02 00:39:13 +0200

    fixed bugs and did refactoring

---
+++ compare_files.cmd
@@ -0,0 +1,2 @@
+py main.py cmp path_a=D:\Git\darkzip\test.txt path_b=D:\Git\darkzip\decode\test.txt
+PAUSE
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 import os
 
+import checker
+
 from archiver.runner import Runner
 
 
 CODE = "c"
+COMPARE = "cmp"
 DECODE = "d"
 EXIT = "e"
 NOT_USE = "-"
@@ -14,31 +17,9 @@ def is_file(file_path):
     return os.path.isfile(file_path)
 
 
-line = " ".join(sys.argv[1:])
-print(line)
-settings = dict()
-is_file_command = False
-runner = Runner()
-params = line.split(maxsplit=1)
-
-if params[0] == CODE:
-    modules = params[1].split('=')
-    for ind, m in enumerate(modules):
-        if m.strip() == "path":
-            if is_file(modules[ind + 1].strip()):
-                settings["file_encode_path"] = modules[ind + 1].strip()
-                is_file_command = True
-            else:
-                settings["folder_encode_path"] = modules[ind + 1].strip()
-
-    if is_file_command:
-        runner.encode_file(**settings)
-    else:
-        runner.encode_folder(**settings)
-
-if params[0] == DECODE:
+def take_settings(modules):
+    settings = dict()
     prev_command = None
-    modules = line.split('=')
     for ind, m in enumerate(modules):
         items = m.split()
         if prev_command is None:
@@ -50,15 +31,61 @@ if params[0] == DECODE:
             if ind != len(items) - 1 or len(items) == 1:
                 curr_param += items[ind] + " "
 
-        if prev_command.strip() == "path":
-            settings["folder_decode_path"] = curr_param.strip()
-        if prev_command.strip() == "out":
-            settings["folder_decode_out_path"] = curr_param.strip()
-
+        settings[prev_command.strip()] = curr_param.strip()
         prev_command = items[-1]
+    return settings
+
+
+line = " ".join(sys.argv[1:])
+print(line)
+settings = dict()
+is_file_command = False
+runner = Runner()
+params = line.split(maxsplit=1)
+
+if params[0] == CODE:
+    modules = params[1].split('=')
+    base_settings = take_settings(modules)
+
+    if "path" in base_settings:
+        if is_file(base_settings["path"]):
+            settings["file_encode_path"] = base_settings["path"]
+            is_file_command = True
+        else:
+            settings["folder_encode_path"] = base_settings["path"]
+
+    if is_file_command:
+        runner.encode_file(**settings)
+    else:
+        runner.encode_folder(**settings)
+
+if params[0] == DECODE:
+    modules = line.split('=')
+    base_settings = take_settings(modules)
+
+    if "path" in base_settings:
+        settings["folder_decode_path"] = base_settings["path"]
+    if "out" in base_settings:
+        settings["folder_decode_out_path"] = base_settings["out"]
 
     runner.decode_folder(**settings)
 
+if params[0] == COMPARE:
+    modules = line.split('=')
+    base_settings = take_settings(modules)
+
+    path_file_a = ""
+    path_file_b = ""
+
+    if "path_a" in base_settings:
+        path_file_a = base_settings["path_a"]
+    if "path_b" in base_settings:
+        path_file_b = base_settings["path_b"]
+
+    if checker.check_files(path_file_a, path_file_b):
+        print("Equal files")
+    else:
+        print("Not equal files")
 
 # def run():
 #     command = NOT_USE
@@ -108,6 +135,35 @@ if params[0] == DECODE:
 #                 prev_command = items[-1]
 #
 #             runner.decode_folder(**settings)
+#
+#         if params[0] == COMPARE:
+#             prev_command = None
+#             modules = line.split('=')
+#             path_file_a = ""
+#             path_file_b = ""
+#
+#             for ind, m in enumerate(modules):
+#                 items = m.split()
+#                 if prev_command is None:
+#                     prev_command = items[-1]
+#                     continue
+#
+#                 curr_param = ""
+#                 for ind in range(len(items)):
+#                     if ind != len(items) - 1 or len(items) == 1:
+#                         curr_param += items[ind] + " "
+#
+#                 if prev_command.strip() == "path_a":
+#                     path_file_a = curr_param.strip()
+#                 if prev_command.strip() == "path_b":
+#                     path_file_b = curr_param.strip()
+#
+#                 prev_command = items[-1]
+#
+#             if checker.check_files(path_file_a, path_file_b):
+#                 print("Equal files")
+#             else:
+#                 print("Not equal files")
 #
 #
 # run()
